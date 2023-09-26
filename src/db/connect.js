96bcf68b8e231c3672fbbe9d7bd6b58292e5224d
Kartik <kartikkhatri2003@gmail.com>
2023-09-26 16:04:34 +0530

    updated

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/haha").then(() => {
+/* mongoose.connect("mongodb://127.0.0.1:27017/haha").then(() => {
     console.log("succesful connection");
 }).catch((e) => {
     console.error("Connection failed:", e);
-})
+}) */
 
-/* mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("MongoDB Connected");
 }).catch((error) => {
     console.error("MongoDB Connection Error:", error);
     process.exit(1);
-});  */
+}); 
--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const { hasSubscribers } = require("diagnostics_channel");
 const { Script } = require("vm");
 
 /* const publicPath = path.resolve(__dirname, "public") */
-const static_path = path.join(__dirname, "../public");
+const static_path = path.join(__dirname, "/mern-backend/public/images");
 const templates_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
@@ -29,6 +29,7 @@ const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.set("view engine", "hbs")
 app.use(express.static(static_path));
